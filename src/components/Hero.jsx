46c2dd0f32463ff792985cb2a0bea991b373dd89
DilanNun <botflowlab@gmail.com>
2025-10-14 16:37:05 -0600

    Added cocktails section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { SplitText } from 'gsap/all';
+import { useRef } from 'react';
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({maxWidth: 767})
 
     useGSAP(() => {
       const heroSplit = new SplitText(".title", { type: "chars, words" });
@@ -37,12 +43,36 @@ const Hero = () => {
       })
       .to('.right-leaf', {y: 200}, 0)
       .to('.left-leaf', {y: -200}, 0)
+
+      const startValue = isMobile ? "top 50%" : "center 60%";
+	    const endValue = isMobile ? "120% top" : "bottom top";
+
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: 'video',
+          start: startValue,
+          end: endValue,
+          scrub: true,
+          pin: true,  
+        }
+      })
+
+      videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current, {
+          currentTime: videoRef.current.duration
+        })
+      }
+
     }, []);
 
   return (
     <>
-      <section id="hero" className="">
-        <h1 className="title text-7xl">Lanza tu sitio web listo para vender</h1>
+      <section id="hero" className="noisy">
+        <h1 className="title">
+          LANZA TU SITIO WEB <div className='text-align right'>
+            LISTO PARA VENDER
+          </div>
+        </h1>
 
         <img src="images/hero-left-leaf.png" alt="left leaf" className="left-leaf" />
         <img src="images/hero-right-leaf.png" alt="right leaf" className="right-leaf" />
@@ -62,6 +92,15 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className='video absolute inset-0'>
+        <video 
+          ref={videoRef}
+          src = '/videos/output.mp4'
+          muted
+          playsInline
+          preload = "auto"
+        />
+      </div>
     </>
   )
 }
