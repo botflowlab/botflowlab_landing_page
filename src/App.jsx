837775e4198af0e4c16577c1f05ef9b91e6a6ba6
botflowlab <botflowlab@gmail.com>
2025-10-14 18:00:55 -0600

    Updated .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import react from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger, SplitText } from "gsap/all";
-import Navbar from './components/navbar';
-import Hero from './components/Hero';
-import Cocktails from './components/Cocktails';
-import About from './components/About';
-import Art from './components/Art';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <main>
-      <Navbar/>
-      <Hero/>
-      <Cocktails />
-      <About />
-      <Art />
-    </main>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
-export default App 
+
+export default App
